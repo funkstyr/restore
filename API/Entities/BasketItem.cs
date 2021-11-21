3ba41375074cf31e4674570046d24fe8af4901df
funkstyr <30911142+funkstyr@users.noreply.github.com>
2021-11-21 07:25:31 -0500

    feat: add basket entity

---
+++ API/Entities/BasketItem.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Entities
+{
+    [Table("BasketItems")]
+    public class BasketItem
+    {
+        public int Id { get; set; }
+
+        public int Quantity { get; set; }
+
+
+        // navigation properties for EF to build relationship
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+
+        public int BasketId { get; set; }
+        public Basket Basket { get; set; }
+    }
+}
